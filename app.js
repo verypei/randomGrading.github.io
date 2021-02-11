0b5e19e582a916fb0e365c8c211b5cbf0102e92c
Hacktiv8 Tech1 <hacktiv8-tech1@Hacktiv8s-MacBook-Air.local>
2021-02-11 15:40:24 +0700

    first

--- app.js
+++ app.js
@@ -1,104 +1,10 @@
-function setupDoubleArrayListInstruktur(listInstruktur,jumlahInstruktur,jumlahSoalLC){
-  // console.log("entering final 2 array");
-  let finalArray = [];
-  let limit = Math.floor(jumlahInstruktur/jumlahSoalLC);
-  let counter = 0;
-  for (let i = 0; i < limit; i++) {
-    let tempArray = [];
-      for (let j = 0; j < jumlahSoalLC; j++) {
-        if(counter > listInstruktur.length-1){
-          break;
-        }
-        else{
-          tempArray.push(listInstruktur[counter]);
-          counter++;
-        }
-      } 
-      finalArray.push(tempArray);//disni gagal
-  }
-  return finalArray;
-}
-
 function makeObject(judulGrading,jumlahSoalLC,jumlahSiswa,noUrut,jumlahInstruktur){
-  // batas no absen
-  let noAbsenAkhir = ((jumlahSiswa+noUrut)-1);
-
-  // template obj akhir
-  let obj = {};
-
-  // ukuran jarak absen
-  const limit = Math.ceil(jumlahSiswa / Math.ceil(jumlahInstruktur / jumlahSoalLC));
-
-  for (let i = 0; i < jumlahSoalLC; i++) {//looping jumlah soal LC
-    let absenAwal = noUrut;//no absen awal
-    let key = `grading ${judulGrading} soal no ${i+1}`;//key objek akhir
-    obj[key] = [];
-    for (let j = 0; j < Math.ceil(jumlahInstruktur/jumlahSoalLC); j++) {//looping untuk pembagian grading
-      let absenAkhir = absenAwal+limit;//limit absen akhir
-      if(absenAwal > noAbsenAkhir){
-        absenAwal = noUrut;
-      }
-      if(absenAkhir > noAbsenAkhir){
-        absenAkhir = noAbsenAkhir;
-      }
-      let objUrutanAbsen = {};
-      let urutanAbsen = `${absenAwal}-${absenAkhir}`;
-      objUrutanAbsen[urutanAbsen] = "";
-      obj[key].push(objUrutanAbsen);
-      absenAwal = absenAkhir+1;
+    let obj = {};
+    for (let i = 0; i < jumlahSoalLC; i++) {
+        let key = `${judulGrading} soal no ${i+1}`;
+        obj[key] = [];
     }
-  }
-  return obj;
-}
-
-// function makeObject(judulGrading,jumlahSoalLC,jumlahSiswa,noUrut,jumlahInstruktur){
-//   let noAwal = noUrut;
-//   let obj = {};
-//   let limit = Math.ceil(jumlahSiswa / jumlahSoalLC);
-//   let batasAkhir = jumlahSiswa+noAwal;
-//   for (let i = 0; i < jumlahSoalLC; i++) {
-//       let counter = 0;
-//       let key  = `${judulGrading} ${i+1}`
-//       obj[key] = [];
-//       let objRange = {}
-//       for (let j = 0; j < Math.floor(jumlahInstruktur/jumlahSoalLC); j++) {
-//           let x = noUrut+limit
-//           if(x > batasAkhir){
-//               x = batasAkhir;
-//           }
-//           let range = `${noUrut}-${x}`;
-//           objRange[range] = "";
-//           noUrut = (x+1);
-//           if(noUrut > batasAkhir){
-//               noUrut = noAwal;
-//           }
-//           counter++;
-//       }
-//       obj[key].push(objRange);
-//   }
-//   return obj;
-// }
-
-
-function setUpInstructorName(obj,arr){
-  // console.log(arr,"---array dalam set up instructor");
-  for (const key in obj) {
-    let counter = 0;
-    for (let i = 0; i < obj[key].length; i++) {
-        for (const key2 in obj[key][0]) {
-            let indexRandom = Math.floor(Math.random()*3);
-            let namaGrader = arr[counter][indexRandom];
-            while(!namaGrader){
-                indexRandom = Math.floor(Math.random()*3);
-                namaGrader = arr[counter][indexRandom];
-            }
-            obj[key][0][key2] = namaGrader;
-            arr[counter] = arr[counter].filter((el)=>{return el!==namaGrader}); 
-            counter++;
-        }
-    }
-  }
-  return obj;
+    return obj;
 }
 
 
@@ -112,32 +18,11 @@ function myFunction(){
   const noUrut = Number(document.getElementById("noUrut").value);
   let jumlahInstruktur = listInstruktur.length;
   let obj = makeObject(judulGrading,jumlahSoalLC,jumlahSiswa,noUrut,jumlahInstruktur);
-  let doubleArrayListInstruktur = setupDoubleArrayListInstruktur(listInstruktur,jumlahInstruktur,jumlahSoalLC);
-  // console.log(listInstruktur,"--->");
-  return [obj,doubleArrayListInstruktur];
+  return obj
 }
 
 function finalRandom(){
   let x = myFunction();
-  let obj = x[0];
-  let arr = x[1]
-  for (const key in obj) {
-    let counter = 0;
-    for (let i = 0; i < obj[key].length; i++) {
-      for (const key2 in obj[key][i]) {
-        console.log(arr[counter].length-1,"----");
-        let indexRandom = Math.floor(Math.random()*arr[counter].length-1);
-        let namaGrader = arr[counter][indexRandom];
-        while(namaGrader === undefined){
-          indexRandom = Math.floor(Math.random()*3);
-          namaGrader = arr[counter][indexRandom];
-        }
-        obj[key][i][key2] = namaGrader;
-        arr[counter] = arr[counter].filter((el)=>{return el!==namaGrader}); 
-        counter++;
-      }
-    }
-  }
+  let obj = x;
   console.log(obj);
-  return obj;
 }
